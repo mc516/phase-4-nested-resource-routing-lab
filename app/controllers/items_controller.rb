b745dee4ebfbf3cc0ebc8837590b3d556c889dfa
Mickey <mickeyc11111@gmail.com>
2022-11-12 15:05:05 -0500

    commit tree b5a840140f713a70ca43e0776bdc12b592744cfe

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,44 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
+
+    render json: Item.all, include: :user
   end
 
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create(item_params)
+   
+
+    render json: item, status: :created
+  end
+
+  def show
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      item = user.items.find(params[:id])
+    else
+      item = Item.find(params[:id])
+    end
+
+    render json: item
+  end
+
+  
+  private
+
+  def render_not_found_response
+    render json: { error: "Item not found" }, status: :not_found
+  end
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
-
   def show
     user = User.find_by(id: params[:id])
-    render json: user, include: :items
+    if user
+      render json: user, include: :items
+    else
+      render json: {error: "No user found"}, status: :not_found
+    end
   end
 
 end
